ee4d788a98d35bde6d15bac26335f6cea92c078a
ZEIAD-LAPTOP\zeiad <zazaoskar928@gmail.com>
2026-06-07 08:29:00 +0300

    Init Commit

---
+++ Duskos/DuskosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuskosApp.swift
+//  Duskos
+//
+//  Created by Zeiad Mohammed on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct DuskosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
